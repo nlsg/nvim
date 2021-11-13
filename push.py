341d9da0f46efe5f5e7294d2e40907dab6067923
nls <kappeler.nils@gmail.com>
2021-11-13 12:53:35 +0100

    added a readme

--- push.py
+++ push.py
@@ -2,5 +2,5 @@
 import os, sys
 os.system("git add -A")
 os.system(f"git commit -m \"{sys.argv[1]}\"")
-os.system("git push https://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@github.com/nlsg/nvim")
+os.system("git push https://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@github.com/nlsg/nvim")
 
